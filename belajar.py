69201d95246613f0982f8d692d1afa319dc9f983
alfathna <143860010+alfathna@users.noreply.github.com>
2023-09-02 11:03:05 +0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,32 @@
+#variabel dan tipe data
+print("Nashh")
+a = 8 #Integer
+l = 4
+k = 3
+i = "alip"
+f = "2005" #String 
+#Float
+b= 3.14
+#Boolean True or false 
+
+#Aritmatika
+
+c = a+l
+print("hasil penjumlahan",c)
+p= a**l
+print("hasil pangkat",p)
+
+#penjumlahan +
+#pengurangan -
+#perkalian *
+#pembagian /
+#pembagian //
+#pangkat **
+#modulus %
+
+persegi_panjang = int(input("masukkan panjang"))
+nama2 = int(input("masukkan lebar"))
+hitung = persegi_panjang * nama2
+
+print("luas persegi panjang adalah",hitung )
+
