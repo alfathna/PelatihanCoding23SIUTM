75c02987fb8f953a221d2da374e032e53d11554b
alfathna <143860010+alfathna@users.noreply.github.com>
2023-09-02 15:32:35 +0700

    Add files via upload

---
+++ oretan.py
@@ -0,0 +1,20 @@
+#VARIABEL DAN TIPE DATA
+#penjumlahan +
+#pengurangan -
+#perkalian *
+#pembagian /
+#pembagian //
+#pangkat **
+#modulus %
+
+
+print("Volume Tabung")
+phi = 3.14
+r = int(input("masukkan jari-jari :"))
+t = int(input("masukkan tinggi :"))
+V = phi * r**2 * t
+print("Rumus Menghitung Volume Tabung : V=3.14xr**2xt")
+print("Volume Tabung = ",V)
+
+
+
